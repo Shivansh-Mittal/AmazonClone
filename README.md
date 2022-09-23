96e2a14df997c45dd230c9f39ea40999762fea34
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-09-23 19:10:48 +0530

    Project ng new again files replaced manually

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# CrudUI
+# AmazonClone
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crudUI'`, () => {
+  it(`should have as title 'AmazonClone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crudUI');
+    expect(app.title).toEqual('AmazonClone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crudUI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AmazonClone app is running!');
   });
 });
